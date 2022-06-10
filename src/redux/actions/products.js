e54e5b4aed6d4ad9f1b827a59bc0304e8bc7d19c
damir-dev-21 <damirtujciev@Air-Damir.corp.siyob.uz>
2022-06-10 10:56:19 +0500

    changed-url

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -4,7 +4,7 @@ import { SET_PRODUCTS, SET_CATALOG, SET_INITIAL_PRODUCTS, SET_PRODUCERS, GET_PRO
 
 export function getProducts() {
     return async (dispatch) => {
-        const responce = await axios.get("http://94.103.83.34:8002/api/items/")
+        const responce = await axios.get("http://127.0.0.1:8000/api/items/")
         let popular = []
         const data = responce.data
         let arr = []
